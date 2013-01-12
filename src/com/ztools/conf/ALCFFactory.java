f117295c257adbcb08ce02ad64d436969a4ce5c6
Zhong Lizhi <zhonglizhi@8chedao.com>
2013-01-12 14:24:06 +0800

    添加通过InputStream来构造AutoConfigurer的方法。

--- src/com/ztools/conf/ALCFFactory.java
+++ src/com/ztools/conf/ALCFFactory.java
@@ -17,103 +17,112 @@ import java.util.Properties;
  */
 public class ALCFFactory {
 
-	private static HashMap<String, AutoConfigurer> producesMap = new HashMap<String, AutoConfigurer>();
+  private static HashMap<String, AutoConfigurer> producesMap = new HashMap<String, AutoConfigurer>();
 
-	public static AutoConfigurer createAutoConfiger(String filePath)
-			throws FileNotFoundException, IOException {
-		AutoConfigurer autoConfiger = producesMap.get(filePath);
-		if (null == autoConfiger) {
-			autoConfiger = new AutoConfigurer(filePath);
-			producesMap.put(filePath, autoConfiger);
-		}
-		return autoConfiger;
-	}
+  public static AutoConfigurer createAutoConfiger(String filePath)
+      throws FileNotFoundException, IOException {
+    AutoConfigurer autoConfiger = producesMap.get(filePath);
+    if (null == autoConfiger) {
+      autoConfiger = new AutoConfigurer(filePath);
+      producesMap.put(filePath, autoConfiger);
+    }
+    return autoConfiger;
+  }
 
-	public static void deleteAutoConfiger(String filePath) {
-		producesMap.remove(filePath);
-	}
+  public static AutoConfigurer createAutoConfiger(InputStream input)
+      throws IOException {
+    AutoConfigurer autoConfiger = producesMap.get("input_" + input.hashCode());
+    if (null == autoConfiger) {
+      autoConfiger = new AutoConfigurer(input);
+      producesMap.put("input_" + input.hashCode(), autoConfiger);
+    }
+    return autoConfiger;
+  }
 
-	public static void deleteAutoConfiger(AutoConfigurer autoConfiger) {
-		deleteAutoConfiger(autoConfiger.getPath());
-	}
-	
-    /**
-     * @param filePath
-     * @param parameterName
-     * @param parameterValue
-     */
-    public static void writeProperties(String filePath,String parameterName,String parameterValue) {
-    	deleteAutoConfiger(filePath);
-        Properties prop = new Properties();
-        InputStream fis = null;
-        OutputStream fos = null;
-        try {
-          fis = new FileInputStream(filePath);
-               prop.load(fis);
-                fos = new FileOutputStream(filePath);
-               prop.setProperty(parameterName, parameterValue);
-               prop.store(fos, "Update '" + parameterName + "' value");
-               
-           } catch (IOException e) {
-            System.err.println("Visit "+filePath+" for updating "+parameterName+" value error");
-           }finally{
-        	   try {
-				if(fos!=null){
-					   fos.close();
-				   }
-				   if(fis!=null){
-					   fis.close();
-				   }
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-           }
-       }
-    
+  public static void deleteAutoConfiger(String filePath) {
+    producesMap.remove(filePath);
+  }
 
-	public static void main(String[] args) {
-		AutoConfigurer autoConfiger = null;
-			try {
-				autoConfiger = ALCFFactory
-						.createAutoConfiger("path/filename");
-			} catch (FileNotFoundException e2) {
-				//û�ҵ��ļ�
-				e2.printStackTrace();
-				System.exit(1);
-			} catch (IOException e2) {
-				// ���쳣
-				System.exit(2);
-				e2.printStackTrace();
-			}
-			autoConfiger.getValue("key");
+  public static void deleteAutoConfiger(AutoConfigurer autoConfiger) {
+    deleteAutoConfiger(autoConfiger.getPath());
+  }
 
-		for (int i = 0; i < 10; i++) {
-			new Thread() {
-				public void run() {
-					while (true) {
-						try {
-							System.out.println(Thread.currentThread().getName()
-									+ " "
-									+ ALCFFactory.createAutoConfiger(
-											"regexp.properties").getValue(
-											"huxiTitle"));
-						} catch (FileNotFoundException e1) {
-							e1.printStackTrace();
-						} catch (IOException e1) {
-							e1.printStackTrace();
-						}
-						try {
-							Thread.sleep(300);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-							// Out.print(
-							// e);
-						}
-					}
-				}
-			}.start();
-		}
-	}
+  /**
+   * @param filePath
+   * @param parameterName
+   * @param parameterValue
+   */
+  public static void writeProperties(String filePath, String parameterName,
+      String parameterValue) {
+    deleteAutoConfiger(filePath);
+    Properties prop = new Properties();
+    InputStream fis = null;
+    OutputStream fos = null;
+    try {
+      fis = new FileInputStream(filePath);
+      prop.load(fis);
+      fos = new FileOutputStream(filePath);
+      prop.setProperty(parameterName, parameterValue);
+      prop.store(fos, "Update '" + parameterName + "' value");
+
+    } catch (IOException e) {
+      System.err.println("Visit " + filePath + " for updating " + parameterName
+          + " value error");
+    } finally {
+      try {
+        if (fos != null) {
+          fos.close();
+        }
+        if (fis != null) {
+          fis.close();
+        }
+      } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    AutoConfigurer autoConfiger = null;
+    try {
+      autoConfiger = ALCFFactory.createAutoConfiger("path/filename");
+    } catch (FileNotFoundException e2) {
+      // û�ҵ��ļ�
+      e2.printStackTrace();
+      System.exit(1);
+    } catch (IOException e2) {
+      // ���쳣
+      System.exit(2);
+      e2.printStackTrace();
+    }
+    autoConfiger.getValue("key");
+
+    for (int i = 0; i < 10; i++) {
+      new Thread() {
+        public void run() {
+          while (true) {
+            try {
+              System.out.println(Thread.currentThread().getName()
+                  + " "
+                  + ALCFFactory.createAutoConfiger("regexp.properties")
+                      .getValue("huxiTitle"));
+            } catch (FileNotFoundException e1) {
+              e1.printStackTrace();
+            } catch (IOException e1) {
+              e1.printStackTrace();
+            }
+            try {
+              Thread.sleep(300);
+            } catch (InterruptedException e) {
+              e.printStackTrace();
+              // Out.print(
+              // e);
+            }
+          }
+        }
+      }.start();
+    }
+  }
 
 }
--- src/com/ztools/conf/AutoConfigurer.java
+++ src/com/ztools/conf/AutoConfigurer.java
@@ -17,123 +17,154 @@ import java.util.concurrent.locks.ReentrantLock;
  * @author Zhong Lizhi
  */
 public class AutoConfigurer implements Serializable {
-    
-    /**
+
+  /**
 	 * 
 	 */
-	private static final long serialVersionUID = -3482941686647354744L;
-	private static final long MIN_RELOAD_TIME = 180000L;
-    private String charSet = "utf-8";
-    
-    private String path = "regexp.properties";
-    private Properties prop = new Properties();
-    
-    private long lastModifyTime = 0L;
-    private long oldCheckTime = System.currentTimeMillis();
-    
-    private ReentrantLock lock = new ReentrantLock();
-    
-    private boolean isReloadComplete = false;
-    
-    public AutoConfigurer(String path) throws FileNotFoundException, IOException {
-        this.path = path;
-        this.loadFile(this.path);
-    }
-    
-    private void loadFile(String path) throws FileNotFoundException,
-            IOException {
-        
+  private static final long serialVersionUID = -3482941686647354744L;
+  private static final long MIN_RELOAD_TIME = 180000L;
+  private String charSet = "utf-8";
+
+  private String path = "regexp.properties";
+  private Properties prop = new Properties();
+
+  private long lastModifyTime = 0L;
+  private long oldCheckTime = System.currentTimeMillis();
+
+  private ReentrantLock lock = new ReentrantLock();
+
+  private boolean isReloadComplete = false;
+
+  public AutoConfigurer(String path) throws FileNotFoundException, IOException {
+    this.path = path;
+    this.loadFile(this.path);
+  }
+
+  public AutoConfigurer(InputStream input) throws IOException {
+    this.path = null;
+    this.loadFile(input);
+  }
+
+  private void loadFile(InputStream input) throws IOException {
+
+    try {
+      lock.lock();
+
+      // Properties p = new
+      // Properties();
+      if (null != input) {
         try {
-            lock.lock();
-            
-            if (isReloadComplete)
-                return;
-            
-            // Properties p = new
-            // Properties();
-            File file = new File(path);
-            if (file.exists()) {
-                lastModifyTime = file.lastModified();
-                InputStream inStream = null;
-                try {
-                    inStream = new FileInputStream(file);
-                    prop.clear();
-                    prop.load(inStream);
-                    isReloadComplete = true;
-                } finally {
-                    if (null != inStream) {
-                        inStream.close();
-                        inStream = null;
-                    }
-                }
-                
-            } else {
-                throw new FileNotFoundException("file path: "
-                        + file.getAbsolutePath());
-            }
+          prop.clear();
+          prop.load(input);
         } finally {
-            lock.unlock();
+          if (null != input) {
+            input.close();
+            input = null;
+          }
         }
-        // return p;
+
+      } else {
+        throw new NullPointerException("input is null");
+      }
+    } finally {
+      lock.unlock();
     }
-    
-    private Properties getProperties() {
-        long currentTime = System.currentTimeMillis();
-        if (MIN_RELOAD_TIME <= currentTime - oldCheckTime) {
-            oldCheckTime = currentTime;
-            File file = new File(path);
-            if (file.exists()) {
-                long fileModifyTime = file.lastModified();
-                if (fileModifyTime != lastModifyTime) {
-                    lastModifyTime = fileModifyTime;
-                    isReloadComplete = false;
-                    try {
-                        loadFile(path);
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
+  }
+
+  private void loadFile(String path) throws FileNotFoundException, IOException {
+
+    try {
+      lock.lock();
+
+      if (isReloadComplete)
+        return;
+
+      // Properties p = new
+      // Properties();
+      File file = new File(path);
+      if (file.exists()) {
+        lastModifyTime = file.lastModified();
+        InputStream inStream = null;
+        try {
+          inStream = new FileInputStream(file);
+          prop.clear();
+          prop.load(inStream);
+          isReloadComplete = true;
+        } finally {
+          if (null != inStream) {
+            inStream.close();
+            inStream = null;
+          }
         }
-        return prop;
+
+      } else {
+        throw new FileNotFoundException("file path: " + file.getAbsolutePath());
+      }
+    } finally {
+      lock.unlock();
     }
-    
-    public void setPath(String path) {
-        if (null == this.path)
-            this.path = path;
-    }
-    
-    public String getPath() {
-        return this.path;
-    }
-    
-    /**当key存在返回对应的值 否则返加null
-     * @param key
-     * @return
-     */
-    public String getValue(String key) {
-    	String tempString =null;
-    	if(getProperties().containsKey(key)){
-    		tempString  = getProperties().getProperty(key);
-    	}else{
-    		return null;
-    	}
-       
-        if (null != tempString) {
-            try {
-                byte[] isoBs = tempString.getBytes("ISO-8859-1");
-                String gbkString = new String(isoBs, charSet);
-                return gbkString.trim();
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
-        } else {
-            Exception e = new NullPointerException("The key is null" + key);
+    // return p;
+  }
+
+  private Properties getProperties() {
+    long currentTime = System.currentTimeMillis();
+    if (null != path && MIN_RELOAD_TIME <= currentTime - oldCheckTime) {
+      oldCheckTime = currentTime;
+      File file = new File(path);
+      if (file.exists()) {
+        long fileModifyTime = file.lastModified();
+        if (fileModifyTime != lastModifyTime) {
+          lastModifyTime = fileModifyTime;
+          isReloadComplete = false;
+          try {
+            loadFile(path);
+          } catch (FileNotFoundException e) {
             e.printStackTrace();
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
         }
-        return null;
+      }
     }
-    
+    return prop;
+  }
+
+  public void setPath(String path) {
+    if (null == this.path)
+      this.path = path;
+  }
+
+  public String getPath() {
+    return this.path;
+  }
+
+  /**
+   * 当key存在返回对应的值 否则返加null
+   * 
+   * @param key
+   * @return
+   */
+  public String getValue(String key) {
+    String tempString = null;
+    if (getProperties().containsKey(key)) {
+      tempString = getProperties().getProperty(key);
+    } else {
+      return null;
+    }
+
+    if (null != tempString) {
+      try {
+        byte[] isoBs = tempString.getBytes("ISO-8859-1");
+        String gbkString = new String(isoBs, charSet);
+        return gbkString.trim();
+      } catch (UnsupportedEncodingException e) {
+        e.printStackTrace();
+      }
+    } else {
+      Exception e = new NullPointerException("The key is null" + key);
+      e.printStackTrace();
+    }
+    return null;
+  }
+
 }
